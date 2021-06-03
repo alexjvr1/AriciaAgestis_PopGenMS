bccd6bd54ff5cfdb96aa34b6cbeae28354ff0707
alexjvr1 <alexjvr1@users.noreply.github.com>
2021-06-03 22:38:00 +0100

    Create SplitVCF.sh

---
+++ SplitVCF.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+###########################################
+# (c) Alexandra Jansen van Rensburg
+# last modified 12/07/2019 05:49 
+###########################################
+
+## Phase vcf file using all bam files
+
+#PBS -N SplitVCF_a  ##job name
+#PBS -l nodes=1:ppn=1  #nr of nodes and processors per node
+#PBS -l mem=16gb #RAM
+#PBS -l walltime=10:00:00 ##wall time.
+#PBS -j oe  #concatenates error and output files (with prefix job1)
+#PBS -t 1-100
+
+#run job in working directory
+cd $PBS_O_WORKDIR
+
+#load modules
+
+module load apps/vcftools-0.1.17.2
+
+##Set up Array
+#Where indivnames is the list of sample names from the vcf file
+#We can only submit 100 samples per array, so we need to set up 3 scripts for ~250 samples
+#bcftools query -l AA.phased.vcf > indivnames
+#VCFFILES is a list of phased.vcf files
+
+NAME=$(sed "${PBS_ARRAYID}q;d" indivnamesaa)
+VCF=$(sed "${PBS_ARRAYID}q;d" VCFFILESaa)
+
+##Run script
+echo "Extracting ${NAME} from ${VCF}"
+printf "\n"
+
+echo "vcftools --vcf ${VCF} --indv ${NAME} --recode --recode-INFO-all --out ${NAME}.split.phased"
+time vcftools --vcf ${VCF} --indv ${NAME} --recode --recode-INFO-all --out ${NAME}.split.phased
